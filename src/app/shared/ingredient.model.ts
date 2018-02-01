4db0b4471add2c6c7db07a8cddf6e9eacc3aba3a
Karan Motani <karan.motani94@gmail.com>
2018-02-01 14:56:39 -0500

    create model for an ingredient

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,15 @@
+export class Ingredient {
+  // public name: string;
+  // public quantity: number;
+  //
+  // constructor(name: string, quantity: number) {
+  //   this.name = name;
+  //   this.quantity = quantity;
+  // }
+
+  // TypeScript Shortcut to assign name and quantity to the property
+  // Same effect as above
+  constructor(public name: string, public quantity: number) {
+
+  }
+}
