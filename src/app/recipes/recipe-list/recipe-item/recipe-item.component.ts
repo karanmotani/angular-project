b51c507cd0320544281f50b7da0c8f8586ba23a4
Karan Motani <karan.motani94@gmail.com>
2018-02-02 12:47:30 -0500

    pass recipe data to single recipe-item using property binding (cross component binding)

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  // Create a property which we want to bind
+  // It should have the same name which we are using in the template to render the data
+  // It should have the same type as of the daat we are rendering in the template
+  @Input() recipe: Recipe;
 
   constructor() { }
 
