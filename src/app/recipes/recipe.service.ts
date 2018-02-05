5d21a2824b04e124f92b91dd9b3ebe3d08850c68
Karan Motani <karan.motani94@gmail.com>
2018-02-05 13:12:51 -0500

    inject shopping list service in recipe service and modify shopping-list service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,13 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
+  constructor(private shoppingListService: ShoppingListService) { }
+
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
@@ -28,5 +32,11 @@ export class RecipeService {
   getRecipes() {
     // Slice - To return a copy of the array instead of the original one
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    // We want to add multiple ingredients to the Ingredients list
+    // Created a new method in shopping-list service for the same
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -24,4 +24,21 @@ export class ShoppingListService {
     // To add the new data and send the copy of the new data
   }
 
+  // We want to add multiple ingredients from the recipe list
+  // Created a new method for the same
+  addIngredients(ingredients: Ingredient[]) {
+  // Downside is that we emit a lot of events, and an addition of even 30 recipes can blow the app
+    // for (let ingredient of ingredients) {
+    //   this.addIngredient(ingredient);
+    // }
+
+    // Better option is to add all ingredients in one go and then emit our event.
+    // ES6 operator - spread operator,
+    // It allows us to turn an arrow of elements -> list of elements
+    // The ... seperates all the ingredients into list of single ingredients
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+
+  }
+
 }
