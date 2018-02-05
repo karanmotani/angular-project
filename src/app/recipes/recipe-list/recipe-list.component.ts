1c0674368173548bb63d6d89483548d75c9fb65d
Karan Motani <karan.motani94@gmail.com>
2018-02-05 10:02:21 -0500

    use the recipes basic service to manage the recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,15 +10,13 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  
-  recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'Description 1', 'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2Fspaghetti-meatballs-tomato-basil-sauce-ck.jpg%3Fitok%3DV5A6yYNW&w=700&q=85'),
-    new Recipe('Test Recipe 2', 'Description 2', 'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2Fspaghetti-meatballs-tomato-basil-sauce-ck.jpg%3Fitok%3DV5A6yYNW&w=700&q=85')
-  ]
 
-  constructor() { }
+  recipes: Recipe[];
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes()
   }
 
   onRecipeSelected(recipeEl: Recipe) {
