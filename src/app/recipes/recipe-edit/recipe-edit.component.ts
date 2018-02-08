d6154071a3a6223c4949aad36fe9e51afb4b48f2
Karan Motani <karan.motani94@gmail.com>
2018-02-08 10:36:20 -0500

    retrieve the id in edit-recipe, check if recipe is in edit mode or new

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe(
+        (param: Params) => {
+          this.id = +param['id'];
+          this.editMode = param['id'] != null;
+          // Check if editMode is false or true
+          // True - can edit the recipe
+          // False - It is a new recipe
+        }
+      )
   }
 
 }
