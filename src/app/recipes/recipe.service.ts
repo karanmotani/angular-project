d66e5e0c06383f03ff37a7cc07445f2fd2de670e
Karan Motani <karan.motani94@gmail.com>
2018-02-08 09:38:14 -0500

    get single recipe using index of array

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -34,6 +34,10 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     // We want to add multiple ingredients to the Ingredients list
     // Created a new method in shopping-list service for the same
